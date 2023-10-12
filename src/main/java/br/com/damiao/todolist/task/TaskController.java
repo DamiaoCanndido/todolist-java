0ec47148daca9436b9ab938dee26ffed60240254
DamiaoCanndido <tower_gamer@hotmail.com>
2023-10-12 15:14:25 -0300

    update task

--- src/main/java/br/com/damiao/todolist/task/TaskController.java
+++ src/main/java/br/com/damiao/todolist/task/TaskController.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.damiao.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -52,5 +55,14 @@ public class TaskController {
         var userId = (UUID)request.getAttribute("userId");
         var tasks = this.taskRepository.findByUserId(userId);
         return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskEntity update(@RequestBody TaskEntity taskEntity, HttpServletRequest request, @PathVariable UUID id){
+        
+        var task = this.taskRepository.findById(id).orElse(null);
+        Utils.copyNonNullProperties(taskEntity, task);
+        
+        return this.taskRepository.save(task); 
     }
 }
