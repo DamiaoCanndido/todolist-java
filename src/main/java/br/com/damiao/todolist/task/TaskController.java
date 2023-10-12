4db6c99769aa642307a99e54bac657279feb2cda
DamiaoCanndido <tower_gamer@hotmail.com>
2023-10-12 12:36:51 -0300

    validating dates

--- src/main/java/br/com/damiao/todolist/task/TaskController.java
+++ src/main/java/br/com/damiao/todolist/task/TaskController.java
@@ -1,5 +1,6 @@
 package br.com.damiao.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,17 @@ public class TaskController {
             return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body("very long title.");
+        }
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskEntity.getStartAt()) || currentDate.isAfter(taskEntity.getEndAt())) {
+            return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body("The start/end date has passed.");
+        }
+        if (taskEntity.getStartAt().isAfter(taskEntity.getEndAt())) {
+            return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body("The end date is smaller than the start date.");
         }
         taskEntity.setUserId((UUID) request.getAttribute("userId"));
         var task = this.taskRepository.save(taskEntity);
